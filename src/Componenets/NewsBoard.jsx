6d650abdb5b319d736b9b14e3fda81872ec1f9e4
RajeshGoura <rajeshgoura96@gmail.com>
2024-04-21 12:18:58 +0530

    changed newsboard

--- src/Componenets/NewsBoard.jsx
+++ src/Componenets/NewsBoard.jsx
@@ -1,23 +1,34 @@
-import { useEffect , useState } from "react";
+import { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 
+const NewsBoard = ({ category }) => {
+  const [articles, setArticles] = useState([]);
 
-const NewsBoard = ({category}) => {
+  useEffect(() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=9c0e2217ca60409a9eb537b8ee2f9fbd`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setArticles(data.articles));
+  }, [category]);
 
-
-    const [articles,setArticles] = useState([]);
-    useEffect(()=>{
-        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=9c0e2217ca60409a9eb537b8ee2f9fbd`;
-        fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
-    },[category])
   return (
     <div>
-        <h2 className="text-center">Latest <span className="badge bg-danger ">News</span></h2>
-        {articles.map((news,index)=>{
-            return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url} />
-        })}
+      <h2 className="text-center">
+        Latest <span className="badge bg-danger">News</span>
+      </h2>
+      {/* Conditionally render the articles only when it's not empty */}
+      {articles.length > 0 &&
+        articles.map((news, index) => (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default NewsBoard
+export default NewsBoard;
